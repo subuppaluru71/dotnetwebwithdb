9fd37a9864bc13445001653453665fedfd651db3
suppaluru-us <s.uppaluru@outlook.com>
2021-06-13 16:29:47 +0530

    connect to SQLDB in Azure

--- Startup.cs
+++ Startup.cs
@@ -28,8 +28,11 @@ namespace DotNetCoreSqlDb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //services.AddDbContext<MyDatabaseContext>(options =>
+             //       options.UseSqlite("Data Source=localdatabase.db"));
+
             services.AddDbContext<MyDatabaseContext>(options =>
-                    options.UseSqlite("Data Source=localdatabase.db"));
+            options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
